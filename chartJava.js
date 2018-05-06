6585483f38da5468b3a260ab19ac62e4cd1aab3d
Manali Bhattacharyya <manalibhattacharyya10@gmail.com>
2018-05-06 06:01:18 +0000

    further eslint corrections and initalized demo index.html page

--- chartJava.js
+++ chartJava.js
@@ -147,18 +147,3 @@ function drawBarChart(data,  options,  element) {
   var cssStyleArray = [barSpace, pos, labelColor, horzLength, titleColor, titleSize, totalHeight];
   cssStyle(cssStyleArray);
 }
-
-var values = [[10, 10, 10], [3], [11], [8], [4, 5], [4, 7, 1, 1], [8], [2]];
-var options =
-{
-  barColors: [["red", "yellow", "pink", "orange", "red", "lightblue"], ["orange"], ["pink", "yellow"], ["yellow"], ["red", "lightblue"], ["green"], ["black"], ["orange"]],
-  barSpace: ["10px"],
-  labelProp: ["green", "top"],
-  title: ["Y as a function of X", "20px", "red"],
-  axisLabel: ["y Axis", "x Axis"],
-  xCat: ["Category1", "Category2", "Cat3", "Cat4", "Cat5", "Cat6", "Cat7", "Cat8"]
-
-};
-var dom = "#chart";
-
-drawBarChart(values, options, dom);
